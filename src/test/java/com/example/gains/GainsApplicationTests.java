26bd9ffdb4bf8a251ab6649bbd4b898690c17dc0
Brian Ji <bji@twilio.com>
2021-06-26 15:19:21 -0700

    demo app

---
+++ src/test/java/com/example/gains/GainsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gains;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GainsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
